782a50905a00a2c9d5ab5761d847618201480a3c
Maxim Goloshhapov <124761114+brellan@users.noreply.github.com>
2025-02-20 22:58:19 +0700

    Add files via upload

---
+++ Practice1/Program1/Program1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = SumOfDigitsOfNumber(1, 100);
+            Console.WriteLine(res);
+        }
+        // метод, который возвращает сумму четных цифр чисел в промежутке от 1 до 100
+        static int SumOfDigitsOfNumber(int left, int right)
+        {
+            int sum = 0;
+            for (int number = left; number <= right; number += 1)
+            {
+                int tmp = Math.Abs(number);
+                while (tmp > 0)
+                {
+                    if (tmp % 2 == 0)
+                        sum += tmp % 10;
+                    tmp /= 10;
+                }
+            }
+            return sum;
+        }
+    }
+}
